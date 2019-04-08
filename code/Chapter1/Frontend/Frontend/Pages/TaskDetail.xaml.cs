76e4034f94704396dc6168886a40572dcea70583
Adrian Hall <photoadrian@outlook.com>
2019-04-08 13:24:53 -0700

    Adding TaskDetail

---
+++ code/Chapter1/Frontend/Frontend/Pages/TaskDetail.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Frontend.Models;
+using Frontend.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Frontend.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskDetail : ContentPage
+    {
+        public TaskDetail(TodoItem item = null)
+        {
+            InitializeComponent();
+            BindingContext = new TaskDetailViewModel(item);
+        }
+    }
+}
